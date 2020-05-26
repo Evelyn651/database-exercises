8a0d1ddee17cf07178e25778759b8439b997596c
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-26 17:14:57 -0500

    feat: added the updates to the file and tested them

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,14 +1,16 @@
 USE codeup_test_db;
 
+select 'The name of all albums by Pink Floyd' AS 'Exercise 1a';
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
+select 'The year Sgt. Peppers Lonely Hearts Club Band' AS 'Exercise 1b';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
 SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
-SELECT name FROM albums WHERE sales <= 20000000;
+SELECT name FROM albums WHERE sales <= 20;
 
 SELECT * FROM albums WHERE genre = 'Rock';
 SELECT * FROM albums WHERE genre = 'Hard Rock';
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,8 +1,14 @@
 USE codeup_test_db;
 -- All albums in your table.
 SELECT name FROM albums;
+UPDATE albums SET sales = sales * 10;
+SELECT name FROM albums;
 -- All albums released before 1980
 SELECT name FROM albums WHERE release_date <=1980;
+UPDATE albums SET release_date;
+SELECT name FROM albums WHERE release_date <=1980;
 -- All albums by Michael Jackson
-SELECT NAME FROM albums WHERE artist = 'Michael Jackson';
+SELECT name FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT artist FROM albums;
 
