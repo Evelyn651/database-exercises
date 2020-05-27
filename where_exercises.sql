3770808b74e366c4346c1c2acb2f9efd4c05b47d
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-27 16:01:05 -0500

    feat: number 4 complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -14,3 +14,8 @@ where first_name in ('Irena', 'Vidya', 'Maya');
 select *
 from employees
 where last_name like 'E%';
+
+# 4.
+select *
+from employees
+where hire_date between '1990-01-01' and '1999-12-31';
