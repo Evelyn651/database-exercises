044fd308834c17d0f82dad693ca78707bda11d85
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-27 16:01:48 -0500

    feat: added alt way of number 4 and completed number 5

--- where_exercises.sql
+++ where_exercises.sql
@@ -19,3 +19,10 @@ where last_name like 'E%';
 select *
 from employees
 where hire_date between '1990-01-01' and '1999-12-31';
+# or
+select * from employees where hire_date like '199%';
+
+# 5.
+select *
+from employees
+where birth_date like '%-12-25';
