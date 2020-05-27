d0cf846ff247e7a62e35ee37fcf0a8547a5431f4
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-27 16:03:23 -0500

    feat: second number 2 done

--- where_exercises.sql
+++ where_exercises.sql
@@ -38,3 +38,11 @@ from employees
 where first_name ='Irena'
    or first_name ='Vidya'
    or first_name = 'Maya';
+
+# 2.
+select *
+from employees
+where (first_name ='Irena'
+    or first_name ='Vidya'
+    or first_name = 'Maya')
+  and gender = 'M';
