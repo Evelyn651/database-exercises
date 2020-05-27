7ad087b853860019c0d8abd231c96ce2112f5603
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-27 16:10:21 -0500

    feat: lecture notes for order by

---
+++ orderby_lecture.sql
@@ -0,0 +1,13 @@
+use employees;
+
+select * from employees order by last_name desc;
+
+select * from employees order by hire_date desc;
+
+select * from employees order by last_name desc, first_name
+
+select * from salaries order by salary desc;
+
+select * from employees where emp_no = 43624;
+select * from titles where emp_no = 43624;
+
