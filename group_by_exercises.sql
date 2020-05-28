6d434f43e636752446a1e59a1ea2ece9bd47bf54
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-28 15:42:24 -0500

    feat: group by exercises done

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -20,5 +20,10 @@ and last_name not like '%qu%'
 group by last_name
 order by last_name;
 
-
+select count(gender), gender
+from employees
+where (first_name ='Irena'
+    or first_name ='Vidya'
+    or first_name = 'Maya')
+  group by gender;
 
