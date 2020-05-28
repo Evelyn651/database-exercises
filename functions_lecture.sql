ca58450571939341d359d9109de7d5e629e0d049
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-28 11:18:18 -0500

    feat: examples with time and date functions

--- functions_lecture.sql
+++ functions_lecture.sql
@@ -10,5 +10,30 @@ select emp_no, birth_date, concat_ws(' ',first_name, last_name, gender) as 'full
 from employees
 limit 10;
 
+-- distinct removes the repeated parts of data and gives only specified data
+select distinct first_name
+from employees
+where first_name like '%su%' limit 10;
+
+select distinct first_name
+from employees
+where first_name not like '%a%'
+order by first_name;
+
+-- date functionality that is built in
+
+select now() as 'current date and time';
+
+select current_date() as 'only the date';
+
+select current_time() as 'only the time';
+
+-- unix_timestamp() function
+select unix_timestamp() as 'how many seconds have passed since january 1, 1970';
+
+select concat('codeup has been teaching people for ',
+    ((((unix_timestamp() - unix_timestamp('2014-02-04'))/ 60) / 60)
+                                                           /24) /365,
+       'years') as 'codeup years';
 
 
