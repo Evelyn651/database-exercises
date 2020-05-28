9ecf9847904fca1d298f9c484db7a45f0a86707a
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-28 09:25:42 -0500

    feat: finished refactoring with order by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -5,7 +5,7 @@ show tables;
 select *
 from employees
 where first_name in ('Irena', 'Vidya', 'Maya')
-order by last_name, first_name;
+order by last_name desc, first_name desc;
 
 # 3.
 select *
@@ -15,14 +15,18 @@ where last_name like 'E%';
 select *
 from employees
 where last_name like '%e%'
-order by emp_no;
+order by emp_no desc;
 
 # 4.
 select *
 from employees
 where hire_date between '1990-01-01' and '1999-12-31';
 # or
-select * from employees where hire_date like '199%';
+select *
+from employees
+where (hire_date like '199%'
+and birth_date like '%-12-25')
+order by birth_date, hire_date desc;
 
 # 5.
 select *
