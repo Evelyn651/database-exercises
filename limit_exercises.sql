8304aa2315993bb742f234b40fac674fa6fc0dcd
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-28 10:32:21 -0500

    feat: added limit exercises file

---
+++ limit_exercises.sql
@@ -0,0 +1,5 @@
+use employees;
+SELECT DISTINCT title FROM titles;
+
+select * from employees
+order by last_name desc limit 10;
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -26,7 +26,7 @@ select *
 from employees
 where (hire_date like '199%'
 and birth_date like '%-12-25')
-order by birth_date, hire_date desc;
+order by birth_date, hire_date desc limit 3;
 
 # 5.
 select *
