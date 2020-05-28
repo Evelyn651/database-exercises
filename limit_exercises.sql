c3ac2e44c1a6c17568896558422ece95f0673846
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-28 10:34:45 -0500

    feat: distinct descending order of last names

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,5 +1,7 @@
 use employees;
 SELECT DISTINCT title FROM titles;
 
-select * from employees
-order by last_name desc limit 10;
+select distinct last_name from employees
+order by last_name desc
+limit 10;
+
