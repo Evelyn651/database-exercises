59e73f345c610a5b0e173d0d9d24e2f008453f6d
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-29 09:14:23 -0500

    feat: lecture notes for joins in mysql.

---
+++ joins_lecture.sql
@@ -0,0 +1,8 @@
+use codeup_test_db;
+
+# i never put these tables in my db so they don work but this is an example of how to join tables
+# select *
+# from quotes as q
+# join authors as a on a.id = q.author_id;
+
+
